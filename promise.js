f24ebb88c1553684ed08d63bc7b29447728fdc56
Sulaksana16 <sulaksana16@gmail.com>
2022-06-12 00:26:01 +0700

    solve challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (mood) => {
+  try {
+    let ixx = await promiseTheaterIXX();
+    let vgc = await promiseTheaterVGC();
+    let merge = ixx.concat(vgc).filter((value) => value.hasil === mood);
+    return merge.length;
+  } catch (err) {
+    return err;
+  }
+}
 
 module.exports = {
   promiseOutput,
